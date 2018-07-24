b167c8d02a18c9f7c4a9facc21ccaee2209907df
Izakk Carrillo <izakkray@gmail.com>
2018-07-24 15:05:01 -0400

    Finish the logic for the Trivia Game's main screen

--- Trivia Game/View Controllers/QuizViewController.swift
+++ Trivia Game/View Controllers/QuizViewController.swift
@@ -29,18 +29,24 @@ class QuizViewController: UIViewController {
         }
     }
     
-    var score = 0 {
+  private var score = 0 {
         didSet {
             //Update the scoreLabel's text whenever the score changes
             scoreLabel.text = "\(score)"
         }
     }
     
+    var questions: [TriviaQuestion] = []
+    
+    var questionsPlaceholder: [TriviaQuestion] = []
+    
+    var currentIndex: Int!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let newQuestion = TriviaQuestion(question: "What color is the sky?", answers: ["Red", "Mauve", "Aubregine", "Blue"], correctAnswerIndex: 3)
-       currentQuestion = newQuestion
+      populateQuestionArray()
+        getNewQuestion()
     }
 
     override func didReceiveMemoryWarning() {
@@ -48,11 +54,53 @@ class QuizViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func populateQuestionArray() {
+         let Question1 = TriviaQuestion(question: "What color is the sky?", answers: ["Red", "Mauve", "Aubregine", "Blue"], correctAnswerIndex: 3)
+         let Question2 = TriviaQuestion(question: "What is 2 + 2?", answers: ["4", "27", "42", "Idk lol"], correctAnswerIndex: 0)
+         let Question3 = TriviaQuestion(question: "What was the first computer?", answers: ["Macintosh", "UNIVAC", "ENIAC", "IBM"], correctAnswerIndex: 1)
+        questions.append(Question1)
+        questions.append(Question2)
+        questions.append(Question3)
+    }
+    
+    func getNewQuestion() {
+        if questions.count > 0 {
+            currentIndex = Int(arc4random_uniform(UInt32(questions.count)))
+            currentQuestion = questions[currentIndex]
+        } else {
+            showGameOverAlert()
+        }
+    }
+    
+    func resetGame() {
+        score = 0
+        
+        if questions.count != 0 {
+            questionsPlaceholder.append(contentsOf: questions)
+            questions.removeAll()
+        }
+        
+        questions = questionsPlaceholder
+        questionsPlaceholder.removeAll()
+        getNewQuestion()
+    }
+    
+    func showGameOverAlert() {
+        let alertController = UIAlertController(title: "Game Over", message: "Your score was \(score).", preferredStyle: .alert)
+        let resetAction = UIAlertAction(title: "Reset", style: .default) { action in
+            self.resetGame()
+        }
+        alertController.addAction(resetAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
     func showCorrectAnswerAlert() {
         let alertController = UIAlertController(title: "Correct!", message: "\(currentQuestion.correctAnswer) is the answer. Good job!", preferredStyle: .alert)
         
         let alertAction = UIAlertAction(title: "Thank you!", style: .default) { action in
-            print("The alert action button was tapped")
+            self.questionsPlaceholder.append(self.questions[self.currentIndex])
+            self.questions.remove(at: self.currentIndex)
+            self.getNewQuestion()
         }
         
         alertController.addAction(alertAction)
@@ -64,8 +112,14 @@ class QuizViewController: UIViewController {
         let alertController = UIAlertController(title: "Incorrect!", message: "\(currentQuestion.correctAnswer) was the correct answer.", preferredStyle: .alert)
         
         let alertAction = UIAlertAction(title: "Oh well...", style: .default) { action in
-            print("The alert action button was tapped")
-    }
+            self.questionsPlaceholder.append(self.questions[self.currentIndex])
+            self.questions.remove(at: self.currentIndex)
+            self.getNewQuestion()
+        }
+        
+        alertController.addAction(alertAction)
+        
+        self.present(alertController, animated: true, completion: nil)
     }
     
     func checkAnswer(selectedButtonTag: Int) {
@@ -82,6 +136,7 @@ class QuizViewController: UIViewController {
     @IBAction func answer1ButtonTapped(_ sender: UIButton) {
         checkAnswer(selectedButtonTag: sender.tag)
     }
+    @IBOutlet weak var resetButtonTapped: UIBarButtonItem!
     
 }
 
