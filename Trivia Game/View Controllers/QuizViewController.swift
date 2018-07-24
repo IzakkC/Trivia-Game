b3f2fee9b31d34686e66515aafc646e6d58bdc8e
Izakk Carrillo <izakkray@gmail.com>
2018-07-24 11:20:13 -0400

    Added in property observers and button

--- Trivia Game/View Controllers/QuizViewController.swift
+++ Trivia Game/View Controllers/QuizViewController.swift
@@ -15,25 +15,71 @@ class QuizViewController: UIViewController {
     @IBOutlet weak var answer2Button: UIButton!
     @IBOutlet weak var answer3Button: UIButton!
     @IBOutlet weak var answer4Button: UIButton!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    //Property Observers
+    var currentQuestion: TriviaQuestion! {
+        didSet {
+            //When currentQuestion is set, update the UI with values from that question
+            questionLabel.text = currentQuestion.question
+            answer1Button.setTitle(currentQuestion.answers[0], for: .normal)
+            answer2Button.setTitle(currentQuestion.answers[1], for: .normal)
+            answer3Button.setTitle(currentQuestion.answers[2], for: .normal)
+            answer4Button.setTitle(currentQuestion.answers[3], for: .normal)
+        }
+    }
+    
+    var score = 0 {
+        didSet {
+            //Update the scoreLabel's text whenever the score changes
+            scoreLabel.text = "\(score)"
+        }
+    }
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         let newQuestion = TriviaQuestion(question: "What color is the sky?", answers: ["Red", "Mauve", "Aubregine", "Blue"], correctAnswerIndex: 3)
-        questionLabel.text = newQuestion.question
-        answer1Button.setTitle(newQuestion.answers[0], for: .normal)
-        answer2Button.setTitle(newQuestion.answers[1], for: .normal)
-        answer3Button.setTitle(newQuestion.answers[2], for: .normal)
-        answer4Button.setTitle(newQuestion.answers[3], for: .normal)
-        
-        print("The correct answer is: \(newQuestion.correctAnswer)")
+       currentQuestion = newQuestion
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func answer1ButtonTapped(_ sender: UIButton) {
+        print(sender.tag)
+    }
+    
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
