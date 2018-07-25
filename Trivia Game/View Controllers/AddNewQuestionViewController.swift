33dc5c8d2eb36955452bca3aa24f7a5ac8933b1f
Izakk Carrillo <izakkcarrillo@skills-macbook-pro-15.local>
2018-07-25 11:28:16 -0400

    Functionality for the AddNewQuestion screen

--- Trivia Game/View Controllers/AddNewQuestionViewController.swift
+++ Trivia Game/View Controllers/AddNewQuestionViewController.swift
@@ -9,7 +9,16 @@
 import UIKit
 
 class AddNewQuestionViewController: UIViewController {
-
+    
+    @IBOutlet weak var questionTextField: UITextField!
+    @IBOutlet weak var answer1TextField: UITextField!
+    @IBOutlet weak var answer2TextField: UITextField!
+    @IBOutlet weak var answer3TextField: UITextField!
+    @IBOutlet weak var answer4TextField: UITextField!
+    @IBOutlet weak var correctAnserSegmentedController: UISegmentedControl!
+    
+    var newQuestion: TriviaQuestion!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -31,5 +40,50 @@ class AddNewQuestionViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    
+    func showErrorAlert() {
+        let errorAlertController = UIAlertController(title:"Error", message: "All text fields must be filled out to make a new question.", preferredStyle: .actionSheet)
+        let alertAction = UIAlertAction(title:"Ok", style: .default, handler: nil)
+        errorAlertController.addAction(alertAction)
+        self.present(errorAlertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: Any) {
+        guard let questionText = questionTextField.text, !questionText.isEmpty else {
+            showErrorAlert()
+            return
+        }
+        
+        guard let firstAnswerText = answer1TextField.text, !firstAnswerText.isEmpty else {
+            showErrorAlert()
+            return
+        }
+        
+        guard let secondAnswerText = answer2TextField.text, !secondAnswerText.isEmpty else {
+            showErrorAlert()
+            return
+        }
+        
+        guard let thirdAnswerText = answer3TextField.text, !thirdAnswerText.isEmpty else {
+            showErrorAlert()
+            return
+        }
+        
+        guard let fourthAnswerText = answer4TextField.text, !fourthAnswerText.isEmpty else {
+            showErrorAlert()
+            return
+        }
+        
+        let selectedSegmentIndex = correctAnserSegmentedController.selectedSegmentIndex
+        
+        newQuestion = TriviaQuestion(question: questionText, answers: [firstAnswerText, secondAnswerText, thirdAnswerText, fourthAnswerText], correctAnswerIndex: selectedSegmentIndex)
+    }
 }
+
+
+
+
+
+
+
+
